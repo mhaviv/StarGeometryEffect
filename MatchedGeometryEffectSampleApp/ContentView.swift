90d61d49377ecef7d82905a1a686396cc603f4b2
Michael Haviv <michael.haviv@fox.com>
2024-09-04 21:15:58 -0400

    Initial Commit

---
+++ MatchedGeometryEffectSampleApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MatchedGeometryEffectSampleApp
+//
+//  Created by Michael Haviv on 9/4/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MatchedGeometryEffectSampleApp/MatchedGeometryEffectSampleAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MatchedGeometryEffectSampleAppApp.swift
+//  MatchedGeometryEffectSampleApp
+//
+//  Created by Michael Haviv on 9/4/24.
+//
+
+import SwiftUI
+
+@main
+struct MatchedGeometryEffectSampleAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
